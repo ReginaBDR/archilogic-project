d8a545c7a80982722141067511c13dd5869de71e
ReginaBDR <reginaborgno013@gmail.com>
2020-09-15 17:39:40 -0300

    starting with space api

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+declare var FloorPlanEngine: any
 
 export default function Space() {
+    useEffect(() => {
+        const sceneId = '940f2267-24a5-446e-8885-4ab76eab2585';
+        const container = document.getElementById('space')
+        const fp = new FloorPlanEngine(container)
+        fp.loadScene(sceneId)/* .then(() => {
+            setSpaces(fp.state.computed.spaces)
+            onSpacesLoaded(fp.state.computed.spaces)
+        }) */
+    }, []);
+
     return (
-        <div>
-            <p>Space</p>
-        </div>
+        <div id="space"></div>
     )
 }
