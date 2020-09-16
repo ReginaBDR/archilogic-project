642562885b1483c07030f80ea1c19c178ca14103
ReginaBDR <reginaborgno013@gmail.com>
2020-09-16 11:22:19 -0300

    corrected Embed and .env

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -2,14 +2,48 @@ import React, {useEffect} from 'react'
 
 declare var FloorPlanEngine: any
 
+const FloorPlanSettings = {
+    hideElements: [],
+    panZoom: true,
+    planRotation: null,
+    roomStampSize: null,
+    ui: {
+        menu: false,
+        scale: false,
+        coordinates: false
+    },
+    theme: {
+        background: {
+            color: '#f3f5f8',
+            showGrid: false
+        },
+        wallContours: false,
+        elements: {
+            roomstamp: { showArea: false }
+        }
+    },
+    units: {
+        system: 'metric',
+        digits: 0,
+        roomDimensions: 'area'
+    }
+}
+
+interface FloorPlanProps {
+    sceneId: string,
+    tickets: any[],
+    onSpacesLoaded: any
+}
+
 export default function Space() {
     useEffect(() => {
         const sceneId = '940f2267-24a5-446e-8885-4ab76eab2585';
         const container = document.getElementById('space')
-        const fp = new FloorPlanEngine(container)
+        const fp = new FloorPlanEngine(container, FloorPlanSettings)
         fp.loadScene(sceneId)/* .then(() => {
             setSpaces(fp.state.computed.spaces)
             onSpacesLoaded(fp.state.computed.spaces)
+            fp.on('click', (event: any) => onRoomClick(event, fp));
         }) */
     }, []);
 
--- src/components/ThreeDimensional.tsx.tsx
+++ src/components/ThreeDimensional.tsx.tsx
@@ -1,29 +1,30 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import ArchilogicEmbed from '@archilogic/embed-api'
 
-const viewer = new ArchilogicEmbed(document.body, {
-    accessToken: process.env.REACT_APP_ARCHILOGIC_PUBLISHABLE_API_KEY,
-    transparentBackground: true,
-    minimap: true,
-    showTitle: true,
-    showLogo: false,
-    uiButtons: { 
-      birdMode: true, 
-      personMode: true,
-      fullscreen: true,
-      bookmarkStrip: true,
-      share: true,
-      help: true,
-      presentation: false
-    }
-  })
 
 export default function ThreeDimensional() {
-    useEffect(() => {
-        const demoSceneId = '940f2267-24a5-446e-8885-4ab76eab2585';
-        viewer.loadScene(demoSceneId)
-    },[]);
-    return (
-        <div id="container"></div>  
-    )
+  const divRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  useEffect(() => {
+    const demoSceneId = '940f2267-24a5-446e-8885-4ab76eab2585';
+    const viewer = new ArchilogicEmbed(divRef.current, {
+      accessToken: process.env.REACT_APP_ARCHILOGIC_PUBLISHABLE_API_KEY,
+      transparentBackground: true,
+      minimap: true,
+      showTitle: true,
+      showLogo: false,
+      uiButtons: {
+        birdMode: true,
+        personMode: true,
+        fullscreen: true,
+        bookmarkStrip: true,
+        share: true,
+        help: true,
+        presentation: false
+      }
+    })
+    viewer.loadScene(demoSceneId)
+  }, [divRef]);
+  return (
+    <div ref={divRef} id="space"></div>
+  )
 }
